e190f3765479c788cbfaedb3d8e755e65030fc79
Paritsu Weeruno <wiwiwawa887@gmail.com>
2025-07-08 05:36:29 +0000

    update

---
+++ LAB-1/ex06.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    int a = 35;
+    printf("Decimal: %d",a);
+    printf("Octal: %o",a);
+    printf("%x",a);
+}
---
+++ LAB/ex02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("My name is Wee$\n");
+    printf("I am %d years old \n",18);
+    printf("i am %f cm tall$ \n",175.66);
+    return 0;
+}
